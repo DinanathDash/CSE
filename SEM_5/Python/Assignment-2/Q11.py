955dae8ff52fb0df0a0f028c2cdd33681129cd71
Dinanath Dash <dashdinanath056@gmail.com>
2024-11-17 20:01:55 +0530

    Update

--- SEM_5/Python/Assignment-2/Q11.py
+++ SEM_5/Python/Assignment-2/Q11.py
@@ -3,14 +3,8 @@ while True:
     if operation == "exit":
         print("Exiting the calculator.")
         break
-    num1 = input("Enter the first number: ")
-    num2 = input("Enter the second number: ")
-    try:
-        num1 = float(num1)
-        num2 = float(num2)
-    except ValueError:
-        print("Invalid input, please enter valid numbers.")
-        continue
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
     if operation == "+":
         result = num1 + num2
     elif operation == "-":
@@ -18,9 +12,6 @@ while True:
     elif operation == "*":
         result = num1 * num2
     elif operation == "/":
-        if num2 == 0:
-            print("Error: Division by zero is not allowed.")
-            continue
         result = num1 / num2
     else:
         print("Invalid operator. Please enter one of +, -, *, /.")
--- SEM_5/Python/Assignment-2/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-if number % 2 == 0:
-    result = number * 2
-else:
-    res
