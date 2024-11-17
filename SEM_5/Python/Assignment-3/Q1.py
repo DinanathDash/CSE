e30160debe608610f67214f185182be4f6866036
Dinanath Dash <dashdinanath056@gmail.com>
2024-11-17 20:59:05 +0530

    Update

--- SEM_5/Python/Assignment-3/Q1.py
+++ SEM_5/Python/Assignment-3/Q1.py
@@ -1,6 +1,6 @@
 def topthree(num):
-    d = [int(digit) for digit in str(num)]
+    d = [int(digit) for digit in num]
     sort = sorted(d, reverse=True)
     return sort[0], sort[1], sort[2]
-num = int(input("Enter a number: "))
+num = input("Enter a number: ")
 print("Output in decreasing order:", topthree(num))
