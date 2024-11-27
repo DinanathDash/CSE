98ff0327b76e6466b5fbb687475a41002b0d4754
Dinanath Dash <dashdinanath056@gmail.com>
2024-11-27 23:10:58 +0530

    Update

--- SEM_5/PPWC/Assignment-4/Q16.c
+++ SEM_5/PPWC/Assignment-4/Q16.c
@@ -6,11 +6,9 @@ int main() {
     int d[3] = {10, 11, 12};
     int *arr[4] = {a, b, c, d};
     int sumarr[3] = {0, 0, 0};
-    for (int i = 0; i < 3; i++) {
-        sumarr[i] = *(arr[0] + i) + *(arr[1] + i) + *(arr[2] + i) + *(arr[3] + i);
-    }
     printf("The element-wise sum of arrays a, b, c, and d is:\n");
     for (int i = 0; i < 3; i++) {
+        sumarr[i] = *(arr[0] + i) + *(arr[1] + i) + *(arr[2] + i) + *(arr[3] + i);
         printf("sumarr[%d] = %d\n", i, sumarr[i]);
     }
     return 0;
