707a7de1e4c3564562f22e6825de81a50cee527a
Dinanath Dash <dashdinanath056@gmail.com>
2025-01-15 20:30:53 +0530

    Update

--- SEM_5/Python/Assignment-7/Q15.py
+++ SEM_5/Python/Assignment-7/Q15.py
@@ -1,23 +1,6 @@
-def substrings_with_frequency(s, frequency):
-    substrings = []
-    current_char = None
-    current_count = 0
-    temp_substring = ""
-    for char in s:
-        if char == current_char:
-            temp_substring += char
-            current_count += 1
-        else:
-            if current_count == frequency:
-                substrings.append(temp_substring)
-            current_char = char
-            current_count = 1
-            temp_substring = char
-    if current_count == frequency:
-        substrings.append(temp_substring)
-
-    return substrings
-string = "aabbbccccddddd"
-freq = 3
-result = substrings_with_frequency(string, freq)
-print(f"Substrings with frequency {freq}: {result}")
+from collections import Counter
+def substrings_with_frequency(s, freq):
+    count = Counter(s)
+    result = [char * freq for char in count if count[char] == freq]
+    print(f"Substrings with frequency {freq}: {', '.join(result) if result else 'None'}")
+substrings_with_frequency('aabbbccccddddd', 3)
