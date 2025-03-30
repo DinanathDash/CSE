9525523b3e94ec87fae347e6061a9d5e54a9bc62
Dinanath Dash <dashdinanath056@gmail.com>
2025-03-30 10:01:49 +0530

    Update

--- SEM_6/Python/Demo T/main.py
+++
@@ -1,6 +0,0 @@
-import key
-import requests
-def user_user_info(username):
-    url = "https://api.twitter.com/2/users/by/username/{username}?.user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,verified_type,withheld"
-    headers = {"Authorization": "Bearer <token>"}
-bt = key.bearer_token
